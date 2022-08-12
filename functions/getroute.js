5da1a41a0ee7057252e5ce799ee6e32db28eac2b
Logan McCalley <mccalleylogan@gmail.com>
2022-08-12 13:54:31 -0400

    back end is finished

--- functions/getroute.js
+++ functions/getroute.js
@@ -1,24 +1,34 @@
 import { dbConnect } from "./dbconnect.js";
-import cors from "cors";
+
 
 
 export function getMessages(req,res){
     const db = dbConnect();
     db.collection('messages').get()
     .then(collection =>{
-        const messages = collection.docs.map(doc.data())
+        console.log(collection);
+        const messages = collection.docs.map(doc => doc.data())
         res.send(messages)
     })
     .catch(err => handleError(err,res))
 }
 
+export function addMessage(req, res) {
+    const db = dbConnect();
+    const message = req.body;
+    const timestamp = FieldValue.serverTimestamp();
+    const newMessage = {...message, timestamp};
+    db.collection('messages').add(newMessage)
+        .then((collection) => {
+            res.send(collection);
+        })
+        .catch (err => handleError(err,res))
+}
+
+    
 
 
 function handleError(err,res){
     console.error(err)
     res.status(500).send(err)
-}
-
-export function addMessage(req, res) {
-
 }
--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,18 @@
-const functions = require("firebase-functions");
+//const functions = require("firebase-functions");
 import  express from 'express';
 import cors from 'cors';
 import {getMessages} from "./getroute.js";
 
 
 const app = express()
-const port = 3003
+const port = 3333;
 app.use(express.json())
 app.use(cors())
 
 app.get('/messages', getMessages)
-app.post('/messages', addMessage)
+//app.post('/messages', addMessage)
 
 app.listen(port, () => {
     console.log('listening')
 })
-export const api = functions.https.onRequest(app)
+//export const api = functions.https.onRequest(app)
