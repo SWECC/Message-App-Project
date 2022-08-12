2bd994d9fbf7a7321b187850d9d01d42ed3fe627
Logan McCalley <mccalleylogan@gmail.com>
2022-08-12 14:12:32 -0400

    updated firebase.json/finished?

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
-//const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 import  express from 'express';
 import cors from 'cors';
-import {getMessages} from "./getroute.js";
+import {getMessages, addMessage} from "./getroute.js";
 
 
 const app = express()
@@ -10,9 +10,9 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/messages', getMessages)
-//app.post('/messages', addMessage)
+app.post('/messages', addMessage)
 
-app.listen(port, () => {
-    console.log('listening')
-})
-//export const api = functions.https.onRequest(app)
+// app.listen(port, () => {
+//     console.log('listening')
+// })
+export const api = functions.https.onRequest(app)
